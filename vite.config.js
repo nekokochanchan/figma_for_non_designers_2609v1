93bcb429c0ce8a176db1edb6910e4e37aef0a6a3
Neko Y. C. <nekokochanchan@gmail.com>
2026-09-15 12:01:30 +0800

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-export default defineConfig({ plugins: [react(), tailwindcss()] });
+
+export default defineConfig({
+  base: '/figma_for_non_designers_2609v1/',
+  plugins: [react(), tailwindcss()],
+});
